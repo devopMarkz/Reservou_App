b02f8725ed519af1533f7270511e9a38dc930695
devopMarkz <marcosdev2002@gmail.com>
2025-08-06 11:01:42 -0300

    First commit

---
+++ api-reservou/src/main/java/com/github/devopMarkz/api_reservou/ApiReservouApplication.java
@@ -0,0 +1,13 @@
+package com.github.devopMarkz.api_reservou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReservouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReservouApplication.class, args);
+	}
+
+}
---
+++ api-reservou/src/test/java/com/github/devopMarkz/api_reservou/ApiReservouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devopMarkz.api_reservou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReservouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
