70696ca1cfc66dc88ae942860492368e3456be18
devopMarkz <marcosdev2002@gmail.com>
2025-08-10 22:18:18 -0300

    Criando gerador de URI

--- api-reservou/src/main/java/com/github/devopMarkz/api_reservou/utils/GeradorDeUri.java
+++ api-reservou/src/main/java/com/github/devopMarkz/api_reservou/utils/GeradorDeUri.java
@@ -1,4 +1,17 @@
 package com.github.devopMarkz.api_reservou.utils;
 
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
 public interface GeradorDeUri {
+
+    static <T> URI generateUri(T id) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+
 }
