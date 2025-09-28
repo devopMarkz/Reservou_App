b50cd59eb10a5cf7d7ede34e9de1f362da454996
devopMarkz <marcosdev2002@gmail.com>
2025-09-28 00:02:55 -0300

    Criand mapper de horario

---
+++ api-reservou/src/main/java/com/github/devopMarkz/api_reservou/horario/infrastructure/HorarioMapper.java
@@ -0,0 +1,56 @@
+package com.github.devopMarkz.api_reservou.horario.infrastructure;
+
+import com.github.devopMarkz.api_reservou.horario.domain.model.Horario;
+import com.github.devopMarkz.api_reservou.horario.domain.repository.HorarioRepository;
+import com.github.devopMarkz.api_reservou.horario.interfaces.dto.HorarioRequestDTO;
+import com.github.devopMarkz.api_reservou.horario.interfaces.dto.HorarioResponseDTO;
+import com.github.devopMarkz.api_reservou.quadra.domain.repository.QuadraRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class HorarioMapper {
+
+    @Autowired
+    private QuadraRepository quadraRepository;
+
+    @Autowired
+    private HorarioRepository horarioRepository;
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "quadra", ignore = true)
+    @Mapping(target = "reservas", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
+    @Mapping(target = "reservado", ignore = true)
+    @Mapping(target = "duracao", ignore = true)
+    public abstract Horario toHorario(HorarioRequestDTO requestDTO);
+
+    @Mapping(target = "idQuadra", expression = "java( getIdFromQuadra(horario) )")
+    @Mapping(target = "reservado", expression = "java ( getReservado(horario) )")
+    @Mapping(target = "duracao", expression = "java ( getDuracaoEmMinutos(horario) )")
+    public abstract HorarioResponseDTO toHorarioResponseDTO(Horario horario);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "quadra", ignore = true)
+    @Mapping(target = "reservas", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
+    @Mapping(target = "reservado", ignore = true)
+    @Mapping(target = "duracao", ignore = true)
+    public abstract Horario toHorarioUpdated(HorarioRequestDTO requestDTO,
+                                             @MappingTarget Horario horario);
+
+    protected Long getIdFromQuadra(Horario horario){
+        return horario.getQuadra().getId();
+    }
+
+    protected Boolean getReservado(Horario horario){
+        return horarioRepository.isHorarioReservadoNoDia(horario, horario.getDataHoraInicio(), horario.getDataHoraFim());
+    }
+
+    protected Long getDuracaoEmMinutos(Horario horario){
+        return horario.getDuracao().toMinutes();
+    }
+
+}
