5cbc5aef6c53847ffb7b78c367770dd33369ee00
devopMarkz <marcosdev2002@gmail.com>
2025-09-30 01:45:21 -0300

    Adicionando indices para tornar consulta mais performatica

--- api-reservou/src/main/resources/db/migration/V2__create_indexes.sql
+++ api-reservou/src/main/resources/db/migration/V2__create_indexes.sql
@@ -6,14 +6,16 @@ CREATE INDEX idx_estabelecimentos_usuario_dono ON tb_estabelecimentos(usuario_do
 CREATE INDEX idx_estabelecimentos_cidade ON tb_estabelecimentos(cidade);
 CREATE INDEX idx_estabelecimentos_estado ON tb_estabelecimentos(estado);
 CREATE INDEX idx_estabelecimentos_cep ON tb_estabelecimentos(cep);
+CREATE INDEX idx_estabelecimentos_nome ON tb_estabelecimentos(nome);
 
 -- Índices para tb_quadras
 CREATE INDEX idx_quadras_estabelecimento ON tb_quadras(estabelecimento_id);
 
 -- Índices para tb_horarios
-CREATE INDEX idx_horarios_quadra ON tb_horarios(quadra_id);
+CREATE INDEX idx_horarios_quadra_ativo ON tb_horarios (quadra_id, ativo);
 CREATE INDEX idx_horarios_data_inicio ON tb_horarios(data_hora_inicio);
 CREATE INDEX idx_horarios_data_fim ON tb_horarios(data_hora_fim);
+CREATE INDEX idx_horarios_reservado ON tb_horarios(reservado);
 
 -- Índices para tb_pedidos (NOVO)
 CREATE INDEX idx_pedidos_usuario ON tb_pedidos(usuario_id);
@@ -22,17 +24,22 @@ CREATE INDEX idx_pedidos_data_pedido ON tb_pedidos(data_pedido);
 
 -- Índices para tb_reservas (AJUSTADO)
 CREATE INDEX idx_reservas_pedido ON tb_reservas(pedido_id); -- Novo índice para a nova relação
-CREATE INDEX idx_reservas_horario ON tb_reservas(horario_id);
-CREATE INDEX idx_reservas_data_reserva ON tb_reservas(data_reserva);
+CREATE INDEX idx_reservas_horario_data ON tb_reservas (horario_id, data_reserva);
+CREATE INDEX idx_reservas_status ON tb_reservas(status);
+
 
 -- Índices para tb_pagamentos (AJUSTADO)
 CREATE INDEX idx_pagamentos_pedido ON tb_pagamentos(pedido_id); -- Novo índice para a nova relação
 CREATE INDEX idx_pagamentos_tipo ON tb_pagamentos(tipo);
 CREATE INDEX idx_pagamentos_status ON tb_pagamentos(status);
 CREATE INDEX idx_pagamentos_data_criacao ON tb_pagamentos(data_criacao);
+CREATE INDEX idx_pagamentos_data_pagamento ON tb_pagamentos(data_pagamento);
 
 -- Índices para tb_avaliacoes
 CREATE INDEX idx_avaliacoes_usuario ON tb_avaliacoes(usuario_id);
 CREATE INDEX idx_avaliacoes_estabelecimento ON tb_avaliacoes(estabelecimento_id);
 CREATE INDEX idx_avaliacoes_nota ON tb_avaliacoes(nota);
 CREATE INDEX idx_avaliacoes_data_criacao ON tb_avaliacoes(data_criacao);
+
+-- Índices para tb_horarios_dias
+CREATE INDEX idx_horarios_dias_horario_dia ON tb_horarios_dias (horario_id, dia_semana);
--- api-reservou/src/main/resources/db/migration/V3__create_more_indexes.sql
+++ api-reservou/src/main/resources/db/migration/V3__create_more_indexes.sql
@@ -1,4 +1,4 @@
-CREATE INDEX idx_estabelecimentos_nome ON tb_estabelecimentos(nome);
-CREATE INDEX idx_horarios_reservado ON tb_horarios(reservado);
-CREATE INDEX idx_reservas_status ON tb_reservas(status);
-CREATE INDEX idx_pagamentos_data_pagamento ON tb_pagamentos(data_pagamento);
+
+
+
+
